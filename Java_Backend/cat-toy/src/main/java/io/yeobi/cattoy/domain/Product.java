ac4ee155dadbd63a04434ca243bd1f99cc12bfb3
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-24 00:04:36 +0900

    Add: 상품 도메인 생성 & getter/setter 구현하여 테스트 통과

---
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/domain/Product.java
@@ -0,0 +1,18 @@
+package io.yeobi.cattoy.domain;
+
+/**
+ * @author yeobi
+ * @created 2019-10-24
+ */
+public class Product {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
