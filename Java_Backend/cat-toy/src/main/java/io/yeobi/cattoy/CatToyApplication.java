d73e585bb8af14f2858a17efb237f2341d8f18c9
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-23 23:03:22 +0900

    Init: cat-toy 프로젝트 생성

---
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/CatToyApplication.java
@@ -0,0 +1,13 @@
+package io.yeobi.cattoy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatToyApplication.class, args);
+	}
+
+}
