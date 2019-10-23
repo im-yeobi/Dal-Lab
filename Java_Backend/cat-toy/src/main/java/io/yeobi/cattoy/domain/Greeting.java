b13bb2419fd4e09245384804d78fbba835765beb
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-24 02:09:51 +0900

    Add: Greeting 도메인 Builder 어노테이션 적용 & getMessage 메소드 구현

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/domain/Greeting.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/domain/Greeting.java
@@ -1,10 +1,22 @@
 package io.yeobi.cattoy.domain;
 
+import lombok.Builder;
+
+import java.util.Objects;
+
 /**
  * @author yeobi
  * @created 2019-10-24
  */
+@Builder
 public class Greeting {
 
     private String name;
+
+    public String getMessage() {
+        if (Objects.isNull(name)) {
+            return "Hello";
+        }
+        return "Hello, " + name;
+    }
 }
