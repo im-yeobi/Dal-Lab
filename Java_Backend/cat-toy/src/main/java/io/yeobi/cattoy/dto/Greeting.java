72dc470cfb93d734acaa2df745849a85c75b81d2
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-23 23:44:13 +0900

    Refactor: Greeting DTO에서 Lombok의 Data 어노테이션 활용하여 getter/setter 자동 생성

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/dto/Greeting.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/dto/Greeting.java
@@ -1,29 +1,15 @@
 package io.yeobi.cattoy.dto;
 
+import lombok.Data;
+
 /**
  * @author yeobi
  * @created 2019-10-23
  * @description 데이터를 주고 받기 위한 DTO 객체
  */
+@Data   // Lombok 플러그인의 @Data 어노테이션 적용하여 getter/setter 사용 (컴파일 단계에서 자동 생성됨)
 public class Greeting {
 
     private String name;
     private String message;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
 }
