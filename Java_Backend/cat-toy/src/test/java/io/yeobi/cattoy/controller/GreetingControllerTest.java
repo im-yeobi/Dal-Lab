34243a7cbcf873e184d0e8f52487ddf24aac41ef
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-24 01:53:33 +0900

    Refactor: MockBean으로 애플리케이션에 레이어에 대한 껍데기 객체 given

--- Java_Backend/cat-toy/src/test/java/io/yeobi/cattoy/controller/GreetingControllerTest.java
+++ Java_Backend/cat-toy/src/test/java/io/yeobi/cattoy/controller/GreetingControllerTest.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,21 +28,53 @@ public class GreetingControllerTest {
     @Autowired
     private MockMvc mockMvc;    // MVC를 테스트 하기 위한 가짜 객체이다.
 
-    @SpyBean
+    // 실제 가짜 객체(껍데기). Spy 객체는 실존하는 객체이나 제어가 용이하도록 침투하는 객체이다.
+    @MockBean
     private GreetingService greetingService;
 
     @Test
     public void hello() throws Exception {
-        mockMvc.perform(get("/hello"))   // Query parameter `name`
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello")));  // Response의 content에 "Hello" 포함되어 있는지 테스트
+        // Given
+        given(greetingService.getMessage(null)).willReturn("Hello");
+
+        // When
+        ResultActions result = mockMvc.perform(
+                get("/hello")
+        );
+
+        // Then
+        result.andExpect(
+                status().isOk()
+        )
+                .andExpect(
+                        content().string(
+                                containsString("Hello")
+                        )
+                );  // Response의 content에 "Hello" 포함되어 있는지 테스트
     }
 
     @Test
     public void helloWithName() throws Exception {
-        mockMvc.perform(get("/hello").param("name", "yeobi"))   // Query parameter `name`
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("yeobi")));
+        // Given
+        given(greetingService.getMessage("yeobi")).willReturn("yeobi");
+
+        // When
+        ResultActions result = mockMvc.perform(
+                get(
+                        "/hello"
+                )
+                        .param("name", "yeobi")
+        );
+
+        // Then
+        result.andExpect(
+                status().isOk()
+        )
+                .andExpect(
+                        content().string(
+                                containsString("yeobi")
+                        )
+                );
     }
 
 }
