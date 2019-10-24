4894a85f1507dd3d0a4f9120a6ee72bb5af4101c
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-24 19:56:51 +0900

    Add: DTO와 Domain model 간의 변형

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductController.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductController.java
@@ -1,5 +1,6 @@
 package io.yeobi.cattoy.controller;
 
+import io.yeobi.cattoy.domain.Product;
 import io.yeobi.cattoy.dto.ProductDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +18,16 @@ public class ProductController {
     @GetMapping("/products")
     public List<ProductDto> list() {
         // TODO: 만들어야 함
+        // DTO 개념이 들어갔다
+        Product product = Product.builder().name("쥐돌이").build();
+
+        List<Product> products = new ArrayList<>();
+        products.add(product);
+
+        // 여기 위는 application에서 처리
+
         ProductDto productDto = new ProductDto();
-        productDto.setName("쥐돌이");
+        productDto.setName(product.getName());
 
         List<ProductDto> productDtos = new ArrayList<>();
         productDtos.add(productDto);
