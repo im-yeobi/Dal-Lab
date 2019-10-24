abd691a66acd75b9a659844def99a6d9af8c9c4c
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-24 20:14:07 +0900

    Add: 상품 Application 레이어에 대한 테스트 통과

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductService.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductService.java
@@ -3,6 +3,7 @@ package io.yeobi.cattoy.controller;
 import io.yeobi.cattoy.domain.Product;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,7 +14,7 @@ import java.util.List;
 public class ProductService {   // 예전에는 클래스를 만드는 것이 아니라 그 사이에 인터페이스를 두었다.
 
     public List<Product> getProducts() {
-        return null;
+        return new ArrayList<>();
     }
 
 }
