ab48113156e4150dd4d9ed016eba3803cc8cffe7
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-24 21:19:30 +0900

    Add: 직접 구현하지 않고 JPA에 의존한다.

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/Repository/ProductRepository.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/Repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package io.yeobi.cattoy.Repository;
 
 import io.yeobi.cattoy.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,8 +10,9 @@ import java.util.List;
  * @author yeobi
  * @created 2019-10-24
  */
+// CrudRepository를 사용할 수도 있지만 페이지네이션 등 JPA의 모든 것 사용하기 위해 JpaRepository 사용
 @Repository
-public interface ProductRepository {
+public interface ProductRepository extends JpaRepository<Product, Long> {   // 도메인 모델 객체, 키.
 
     List<Product> findAll();
 
--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/domain/Product.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/domain/Product.java
@@ -3,6 +3,8 @@ package io.yeobi.cattoy.domain;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.persistence.Entity;    // javax : java 표준에서의 확장
+import javax.persistence.Id;
 import java.text.NumberFormat;
 import java.util.Objects;
 
@@ -10,8 +12,12 @@ import java.util.Objects;
  * @author yeobi
  * @created 2019-10-24
  */
+@Entity // JPA에서 잡아주는 것이다
 @Builder
 public class Product {
+
+    @Id
+    private Long id;
 
     @Getter
     private String name;
