6f00fa22a9aa0635d665b85e584d0b2f22fe4cbb
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-24 20:02:11 +0900

    Add: Product 도메인의 List형식을 Stream을 이용하여 DTO List로 변경
    ProductService에 대한 의존성을 주입하지 않았기 때문에 테스트 시에 오류가 발생한다.

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductController.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductController.java
@@ -2,11 +2,12 @@ package io.yeobi.cattoy.controller;
 
 import io.yeobi.cattoy.domain.Product;
 import io.yeobi.cattoy.dto.ProductDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author yeobi
@@ -15,24 +16,28 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/products")
     public List<ProductDto> list() {
         // TODO: 만들어야 함
         // DTO 개념이 들어갔다
-        Product product = Product.builder().name("쥐돌이").build();
-
-        List<Product> products = new ArrayList<>();
-        products.add(product);
+//        Product product = Product.builder().name("쥐돌이").build();
+//
+//        List<Product> products = new ArrayList<>();
+//        products.add(product);
 
         // 여기 위는 application에서 처리
 
-        ProductDto productDto = new ProductDto();
-        productDto.setName(product.getName());
+        List<Product> products = productService.getProducts();  // List를 또 다른 List로 바꾸기 위해 Stream을 사용한다.
 
-        List<ProductDto> productDtos = new ArrayList<>();
-        productDtos.add(productDto);
-
-        return productDtos;
+        // Stream => functional program
+        return products.stream().map(product -> {
+            ProductDto productDto = new ProductDto();
+            productDto.setName(product.getName());
+            return productDto;
+        }).collect(Collectors.toList());
     }
 
 
---
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductService.java
@@ -0,0 +1,19 @@
+package io.yeobi.cattoy.controller;
+
+import io.yeobi.cattoy.domain.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author yeobi
+ * @created 2019-10-24
+ */
+@Service
+public class ProductService {   // 예전에는 클래스를 만드는 것이 아니라 그 사이에 인터페이스를 두었다.
+
+    public List<Product> getProducts() {
+        return null;
+    }
+
+}
