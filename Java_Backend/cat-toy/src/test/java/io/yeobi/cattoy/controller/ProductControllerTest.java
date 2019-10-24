907a01d78498c41ee97077a3c29bfba9df378aad
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-24 19:44:21 +0900

    Add: /products API에 대한 실패하는 테스트 코드 작성
    에러가 나는 상황을 먼저 확인한다.

--- Java_Backend/cat-toy/src/test/java/io/yeobi/cattoy/controller/ProductControllerTest.java
+++ Java_Backend/cat-toy/src/test/java/io/yeobi/cattoy/controller/ProductControllerTest.java
@@ -1,9 +1,33 @@
 package io.yeobi.cattoy.controller;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 /**
  * @author yeobi
  * @created 2019-10-24
  */
+@RunWith(SpringRunner.class)
+@WebMvcTest
 public class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    /**
+     * GET /products
+     */
+    @Test
+    public void list() throws Exception {
+        mockMvc.perform(get("/products"))
+                .andExpect(status().isOk());
+    }
 
 }
