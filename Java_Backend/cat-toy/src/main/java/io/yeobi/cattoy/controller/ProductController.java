cf21f0749449b177605dc682bcf34b265349a7cf
im-yeobi <dev.jongyeobKim@gmail.com>
2019-10-29 19:56:03 +0900

    Refactor: mapper 사용

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductController.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/controller/ProductController.java
@@ -1,5 +1,7 @@
 package io.yeobi.cattoy.controller;
 
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
 import io.yeobi.cattoy.domain.Product;
 import io.yeobi.cattoy.dto.ProductDto;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,24 +22,15 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/products")
-    public List<ProductDto> list() {
-        // TODO: 만들어야 함
-        // DTO 개념이 들어갔다
-//        Product product = Product.builder().name("쥐돌이").build();
-//
-//        List<Product> products = new ArrayList<>();
-//        products.add(product);
-
-        // 여기 위는 application에서 처리
+    public List<ProductDto> list() {    // 반환형을 Stream으로 할 수도 있다.
+        Mapper mapper = DozerBeanMapperBuilder.buildDefault();
 
         List<Product> products = productService.getProducts();  // List를 또 다른 List로 바꾸기 위해 Stream을 사용한다.
 
         // Stream => functional program
-        return products.stream().map(product -> {
-            ProductDto productDto = new ProductDto();
-            productDto.setName(product.getName());
-            return productDto;
-        }).collect(Collectors.toList());
+        return products.stream()
+                .map(product -> mapper.map(product, ProductDto.class))
+                .collect(Collectors.toList());
     }
 
 
