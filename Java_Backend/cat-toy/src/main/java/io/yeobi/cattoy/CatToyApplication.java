fe5dd78f0fad2d1aa3c160c4c38523eb4f3114dd
im-yeobi <dev.jongyeobKim@gmail.com>
2019-11-05 20:26:45 +0900

    Add: JWT secret 키 환경변수 등록

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/CatToyApplication.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/CatToyApplication.java
@@ -2,12 +2,16 @@ package io.yeobi.cattoy;
 
 import com.github.dozermapper.core.DozerBeanMapperBuilder;
 import com.github.dozermapper.core.Mapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CatToyApplication {
+
+	@Value("${jwt.secret}")	// yml 설정으로부터 JWT secret키 읽어온다
+	private String secret;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CatToyApplication.class, args);
