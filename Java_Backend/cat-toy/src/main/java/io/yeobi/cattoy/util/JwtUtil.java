532d616a896f972e1c6685e34c936df7f96c4b82
im-yeobi <dev.jongyeobKim@gmail.com>
2019-11-05 20:10:44 +0900

    Update: JWT 토큰 생성

--- Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/util/JwtUtil.java
+++ Java_Backend/cat-toy/src/main/java/io/yeobi/cattoy/util/JwtUtil.java
@@ -1,11 +1,32 @@
 package io.yeobi.cattoy.util;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+
 /**
  * @author yeobi
  * @created 2019-11-05
  */
 public class JwtUtil {
+    // 256 / 8 = 32자리 수보다 커야한다.
+    private final String secret;
+
+    public JwtUtil(String secret) {
+        this.secret = secret;
+    }
+
     public String createToken(long userId, String name) {
-        return null;
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+        String token = Jwts.builder()
+                .claim("userId", userId)
+                .claim("name", name)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
+        return token;
     }
 }
--- Java_Backend/cat-toy/src/test/java/io/yeobi/cattoy/util/JwtUtilTest.java
+++ Java_Backend/cat-toy/src/test/java/io/yeobi/cattoy/util/JwtUtilTest.java
@@ -10,9 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class JwtUtilTest {
 
+    private static final String SECRET = "12345678901234567890123456789012";
+
     @Test
     public void createToken() {
-        JwtUtil jwtUtil = new JwtUtil();
+        JwtUtil jwtUtil = new JwtUtil(SECRET);
 
         String token = jwtUtil.createToken(13L, "테스터");
 
